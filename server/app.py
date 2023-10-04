cb913efea22d3b27f58291c28a07a5e92ba814a6
= <=>
2023-10-04 00:24:41 -0400

    complete lab

--- server/app.py
+++ server/app.py
@@ -48,6 +48,31 @@ class ShowArticle(Resource):
 
         return {'message': 'Maximum pageview limit reached'}, 401
 
+class Login(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User.query.filter(User.username == data.get('username')).first()
+        session['user_id'] = user.id
+        response = make_response(user.to_dict(), 200)
+        return response
+
+class Logout(Resource):
+    def delete(self):
+        session['user_id'] =None
+        message = {'message':'Logged out'}
+        return make_response(message, 204)
+
+class CheckSession(Resource):
+    def get(self):
+        user = User.query.filter(User.id == session.get('user_id')).first()
+        if user:
+            return make_response(user.to_dict(), 200)
+        else:
+            return make_response({}, 401)
+
+api.add_resource(Login, '/login')
+api.add_resource(Logout, '/logout')
+api.add_resource(CheckSession, '/check_session')
 api.add_resource(ClearSession, '/clear')
 api.add_resource(IndexArticle, '/articles')
 api.add_resource(ShowArticle, '/articles/<int:id>')
